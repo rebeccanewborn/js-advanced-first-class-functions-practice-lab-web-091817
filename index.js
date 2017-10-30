fe0d3b23dfcaa32481d6bff588049b77b86c0b9e
rebeccanewborn <rjn58@cornell.edu>
2017-10-30 16:53:16 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function logDriverNames(arr) {
+  arr.forEach(function(driver) {
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(arr, hometown) {
+  function fromHometown(driver) {
+    return driver.hometown === hometown;
+  }
+
+  const filtered = arr.filter(fromHometown);
+
+  filtered.forEach(function(driver) {
+    console.log(driver.name);
+  });
+}
+
+function driversByRevenue(drivers) {
+  function revenueSorter(driver1, driver2) {
+    return driver1.revenue - driver2.revenue;
+  }
+  return drivers.slice().sort(revenueSorter);
+}
+
+function driversByName(drivers) {
+  function nameSorter(driver1, driver2) {
+    return driver1.name.localeCompare(driver2.name);
+  }
+  return drivers.slice().sort(nameSorter);
+}
+
+function totalRevenue(drivers) {
+  function reduceRevenues(agg, el, i, arr) {
+    return agg + el.revenue;
+  }
+  return drivers.reduce(reduceRevenues, 0);
+}
+
+function averageRevenue(drivers) {
+  const totalRev = totalRevenue(drivers);
+  return totalRev / drivers.length;
+}
